6e6532cf5fe0bf1d965910056ea8b693d6356608
SevenZero <iamseventoodyai708090sz@gmail.com>
2024-10-05 22:17:28 +0700

    Revert "Update MovingCreatures.cs"
    This reverts commit 4b125a5e35136690ece3d553a48103e0d06bdc1a.

--- Assets/Scripts/MovingCreatures.cs
+++ Assets/Scripts/MovingCreatures.cs
@@ -1,5 +1,5 @@
+using Unity.VisualScripting;
 using UnityEngine;
-using System.Collections.Generic;
 
 public class MovingCreatures : MonoBehaviour
 {
@@ -12,36 +12,11 @@ public class MovingCreatures : MonoBehaviour
 
     float moveSpeed = 4;
     [HideInInspector] public Vector3 rotation = Vector3.down;
-    public float followDistance = 1f;
-
     void Update()
-    {
-        MoveCreature();
-        FollowLeader();
-    }
-
-    void MoveCreature()
     {
         transform.position += rotation * moveSpeed * Time.deltaTime;
     }
 
-    void FollowLeader()
-    {
-        int index = GameStates.instance.creatureStorage.IndexOf(gameObject);
-
-        if (index > 0)
-        {
-            GameObject leader = GameStates.instance.creatureStorage[index - 1];
-            Vector3 leaderPosition = leader.transform.position;
-
-            Vector3 directionToLeader = (leaderPosition - transform.position).normalized;
-
-            if (Vector3.Distance(transform.position, leaderPosition) > followDistance)
-            {
-                transform.position += directionToLeader * moveSpeed * Time.deltaTime;
-            }
-        }
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -50,24 +25,28 @@ public class MovingCreatures : MonoBehaviour
             transform.position = collision.transform.position;
             rotation = Vector3.right;
             GameStates.instance.rotationObjectStorage[collision.gameObject]--;
+
         }
-        else if (collision.gameObject.tag == "TURNLEFT")
+        if (collision.gameObject.tag == "TURNLEFT")
         {
             transform.position = collision.transform.position;
             rotation = Vector3.left;
             GameStates.instance.rotationObjectStorage[collision.gameObject]--;
+
         }
-        else if (collision.gameObject.tag == "TURNUP")
+        if (collision.gameObject.tag == "TURNUP")
         {
             transform.position = collision.transform.position;
             rotation = Vector3.up;
             GameStates.instance.rotationObjectStorage[collision.gameObject]--;
+
         }
-        else if (collision.gameObject.tag == "TURNDOWN")
+        if (collision.gameObject.tag == "TURNDOWN")
         {
             transform.position = collision.transform.position;
             rotation = Vector3.down;
             GameStates.instance.rotationObjectStorage[collision.gameObject]--;
+
         }
     }
 }
